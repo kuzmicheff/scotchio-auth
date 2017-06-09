8e63415df8090a00f251e1ad567f57554cde6ec5
kuzmicheff <kuzmicheff@gmail.com>
2017-06-09 14:01:05 -0500

    Set up app, added views

---
+++ config/database.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+var mongoose = require('mongoose');
+
+var dbURI = "mongodb://" + process.env.dbUser 
+                         + ":" 
+                         + process.env.dbPass 
+                         + "@" 
+                         + process.env.dbHost;
+
+module.exports = {
+  "url": dbURI
+}
