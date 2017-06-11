bb67cbf5b323b44cfbb3bfee1b6bb2a56384fe16
kuzmicheff <kuzmicheff@gmail.com>
2017-06-11 10:52:28 -0500

    Implemented Facebook authentication

---
+++ config/auth.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+  'facebookAuth': {
+    'clientID':      process.env.fbAppId,
+    'clientSecret':  process.env.fbAppSecret,
+    'callbackURL':   'http://localhost:8080/auth/facebook/callback',
+    'profileURL':    'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+  },
+
+  'twitterAuth': {
+    'consumerKey':    process.env.twAppId,
+    'consumerSecret': process.env.twAppSecret,
+    'callbackURL':    'http://localhost:8080/auth/twitter/callback'
+  },
+
+  'googleAuth': {
+    'clientID':     process.env.glAppId,
+    'clientSecret': process.env.glAppSecret,
+    'callbackURL':  'http://localhost:8080/auth/google/callback'
+  }
+
+};
--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,8 @@
-var LocalStrategy = require('passport-local').Strategy;
-var User =          require('../app/models/user');
+var LocalStrategy =    require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var User =             require('../app/models/user');
+
+var authConfig = require('./auth');
 
 module.exports = function(passport) {
   
@@ -14,8 +17,8 @@ module.exports = function(passport) {
   });
 
   passport.use('local-signup', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
+    usernameField:     'email',
+    passwordField:     'password',
     passReqToCallback: true
   },
   function(req, email, password, done) {
@@ -44,8 +47,8 @@ module.exports = function(passport) {
   }));
 
   passport.use('local-login', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
+    usernameField:     'email',
+    passwordField:     'password',
     passReqToCallback: true
   },
   function(req, email, password, done) {
@@ -61,5 +64,66 @@ module.exports = function(passport) {
       }
       return done(null, user);
     });
+  }));
+
+  var fbAuthConfig = authConfig.facebookAuth;
+  fbAuthConfig.passReqToCallback = true;
+
+  passport.use(new FacebookStrategy(fbAuthConfig,
+    function(req, token, refreshToken, profile, done) {
+      // asynchronous
+      process.nextTick(function() {
+        // check if the user is already logged in
+        if (!req.user) {
+          User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+            if (err) {
+              return done(err);
+            }
+            if (user) {
+              // if there is a user id already but no token (user was linked at one point and then removed)
+              if (!user.facebook.token) {
+                user.facebook.token = token;
+                user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                user.facebook.email = (profile.emails[0].value || '').toLowerCase();
+                user.save(function(err) {
+                  if (err) {
+                    return done(err);
+                  }
+                  return done(null, user);
+                });
+              }
+              return done(null, user); // user found, return that user
+            } 
+            else {
+              // if there is no user, create new user
+              var newUser            = new User();
+              newUser.facebook.id    = profile.id;
+              newUser.facebook.token = token;
+              newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+              newUser.facebook.email = (profile.emails[0].value || '').toLowerCase();
+              newUser.save(function(err) {
+                if (err) {
+                  return done(err);
+                }
+                return done(null, newUser);
+              });
+            }
+          });
+        } 
+        else {
+          // user already exists and is logged in, we have to link accounts
+          var user            = req.user; // pull the user out of the session
+          user.facebook.id    = profile.id;
+          user.facebook.token = token;
+          user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+          user.facebook.email = (profile.emails[0].value || '').toLowerCase();
+          user.save(function(err) {
+            if (err) {
+              return done(err);
+            }
+            return done(null, user);
+          });
+        }
+      });
   }));
 };
